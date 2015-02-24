ef305bf7586066505d15a83cfcd856109e54fcf9
Roy Riojas <roy@kno.com>
2015-02-24 14:31:57 -0800

    BLD: Fist commit

---
+++ test/fixtures/basic.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var fs = require( 'fs' );
+var utils = require( './lib/util' );
+var path = require( 'path' );
+
+var showErrorBlock      = utils.showErrorBlock;
+var showTitleBlock =      utils.showTitleBlock;
+var showSuccessBlock =    utils.showSuccessBlock;
+
+var commitMessageFile =     process.argv[ 2 ];
+
+showTitleBlock( 'Checking commit message' );
+
+var cfg = require( './lib/hooks-cfg.json' );
+
+var content =     fs.readFileSync(    commitMessageFile, {
+  encoding: 'utf8'
+});
